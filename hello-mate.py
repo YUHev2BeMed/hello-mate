247215a864615b67bf09b6688028e0e6145f4594
Jacob Ferrier <reallylookin2022@outlook.com>
2023-02-02 13:46:34 -0700

    Create hello-mate.py

---
+++ hello-mate.py
@@ -0,0 +1,26 @@
+# this program asks for my name and says hello 'name'.
+
+
+
+print("'Ello, mate!")
+print("what is your name") # asks for name
+myName= input()
+
+print('Its nice to meet you, ' + myName)
+
+print('The length of your name is:')
+print(len(myName))
+
+print('How old are you?') # asks for age
+
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
+print('Good bye, you are now entering the Python interpreter.')
+print('do you want to enter the interpreter?')
+myAnswer= input()
+print("well go anyway, you " + myAnswer + " person!")
+print('   Made by Jacob Ferrier, who is YUHev2BeMed to you!   ')
+
+
+print('The End!')
